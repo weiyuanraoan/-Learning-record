a1dd3ff89dcf9e1f8502416b3215b43e07d5aa9d
weiyuanraoan <1141340018@qq.com>
2019-11-25 22:23:53 +0800

    nosql

---
+++ mongodb/nosql/main.js
@@ -0,0 +1,86 @@
+// // const express = require('express');//引入 node 开发框架 
+// // // 异步无阻塞 node 可以支持更高的并发
+// // //启动一个webapp
+// // const app = express();
+// // app.get('/',(req,res)=>
+// // res.send('hello world'))
+// // app.listen(3000,()=>
+// // console.log('Example app listening on port 3000!'));
+
+// const express = require('express');
+// var MongoClient = require('mongodb').MongoClient;
+// // mongodb 连接是用mongodb:// 协议来  27017端口
+// // mysql 3306
+// var url = 'mongodb://192.168.31.128:27017/runoob';
+// var dbbase;
+// MongoClient.connect(url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }, function(err, db) {
+//   if (err) {
+//     throw err;
+//   }
+//   //console.log('数据库已创建！');
+//   var dbbase = db.db("runoob");
+// })
+// //模板引擎
+// const ejs = require('ejs');
+// const app = express();
+// app.get('/',(req,res)=> {
+//     dbbase.collection("pzw_site")
+//     .find({"name":"潘志伟"})
+//     .toArray(function(err,result){
+//         if(err){ throw err;}
+//         console.log(result);
+//         // db.close(); 
+//         ejs.renderFile("views/index.html",{
+//         title:'hello'
+//     },function(err,data){
+//         res.end(data);
+//     })
+//     })
+//     // res.end(data);
+   
+// })
+// app.listen(3000);
+
+const express = require('express');
+//模板引擎
+const ejs = require('ejs');
+const app = express();
+var MongoClient = require('mongodb').MongoClient;
+// mongodb 连接是用mongodb:// 协议来  27017端口
+// mysql 3306
+var url = 'mongodb://192.168.31.128:27017/runoob';
+var dbbase;
+MongoClient.connect(url, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}, function(err, db) {
+  if (err) {
+    throw err;
+  }
+  // console.log('数据库已创建！');
+  dbbase = db.db("runoob");
+  // console.log(dbbase);
+});
+app.get('/', (req, res) => {
+  dbbase.collection("ysw_site")
+    .find({"name":"潘志伟"})
+    .toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      // db.close();
+      ejs.renderFile("views/index.html", {
+        title: 'hello',
+        items:result
+      }, function(err, data) {
+        res.end(data);
+      })
+
+
+    })
+
+  
+})
+app.listen(3000);
