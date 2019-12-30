aec56710f8da00545029894a515c8c124bf12c39
weiyuanraoan <1141340018@qq.com>
2019-12-30 21:43:18 +0800

    music

---
+++ vue/163music/README.md
@@ -0,0 +1,10 @@
+- 图标库 
+  <后退
+  FontAwesome 
+
+- 在common/styl/variable.styl
+  全局变量 
+  $name = value
+
+- 在vue中 父组件复杂通过请求来请求提供数据 
+  子组件 通过props 申请消费数据
---
+++ vue/163music/src/components/switches/switches.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul class="switches">
+  <li
+    v-for="(item,index) in switches"
+    :key= "item.id"
+    class="switch-item"
+    :class = "{'active':currentIndex === index}"
+    @click="switchIndex(index)"
+    >
+    <span>{{item.name}}</span>
+  </li>
+</ul>
+</template>
+
+<script>
+export default {
+    props:{
+        switches:{
+            type:Array
+        },
+        currentIndex:{
+            type:Number,
+            default:0
+        }
+        },
+    methods:{
+        switchIndex(index){
+            this.$emit('switch',index)
+        }
+    
+    }
+
+}
+</script>
+
+<style lang= "stylus">
+@import "../../common/styl/variable"
+.switches
+    display flex
+    align-items center
+    width 240px
+    margin 0 auto 
+    .switch-item
+        flex 1
+        padding 8px
+        margin 0 20px
+        text-align center
+        font-style 16px
+        color $color-text-l
+        border-bottom 2px solid $color-theme
+        &.active
+            color #ffffff
+            border-bottom 2px solid $color-text-l
+</style>
